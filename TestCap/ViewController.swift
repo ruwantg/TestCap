3362dfada8c4b2999aa9d61704e32ceb6c612903
Ruwan Thalgahage <ruwantgs@gmail.com>
2024-05-17 21:29:59 -0400

    Initial Commit

---
+++ TestCap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestCap
+//
+//  Created by Ruwan Thalgahage on 2024-05-17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
